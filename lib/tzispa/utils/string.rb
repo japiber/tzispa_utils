48819cfe707b98bc152bc8cc49c379ad84a0b5e7
Juan Antonio Piñero <japinero@area-integral.com>
2016-03-09 12:34:58 +0100

    Make Tzispa::Utils::String a subclass of ::String for better usability

--- lib/tzispa/utils/string.rb
+++ lib/tzispa/utils/string.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+require 'i18n'
+
 module Tzispa
   module Utils
 
-
-
-    class String
+    class String < ::String
 
       NAMESPACE_SEPARATOR        = '::'
       CLASSIFY_SEPARATOR         = '_'
@@ -13,51 +13,12 @@ module Tzispa
 
       UNDERSCORE_DIVISION_TARGET = '\1_\2'
 
-      # The extended characters map used by removeaccents. The accented characters
-      # are coded here using their numerical equivalent to sidestep encoding issues.
-      # These correspond to ISO-8859-1 encoding.
-      ACCENTS_MAPPING = {
-        'E' => [200,201,202,203],
-        'e' => [232,233,234,235],
-        'A' => [192,193,194,195,196,197],
-        'a' => [224,225,226,227,228,229,230],
-        'C' => [199],
-        'c' => [231],
-        'O' => [210,211,212,213,214,216],
-        'o' => [242,243,244,245,246,248],
-        'I' => [204,205,206,207],
-        'i' => [236,237,238,239],
-        'U' => [217,218,219,220],
-        'u' => [249,250,251,252],
-        'N' => [209],
-        'n' => [241],
-        'Y' => [221],
-        'y' => [253,255],
-        'AE' => [306],
-        'ae' => [346],
-        'OE' => [188],
-        'oe' => [189]
-      }
-
-
-      def initialize(str)
-        @string = str.to_s
-      end
-
-      def to_s
-        @string
-      end
-
-      def ==(other)
-        to_s == other
-      end
-
       def self.constantize(str)
-        self.new(str).constantize
+        self.new(str.to_s).constantize
       end
 
       def constantize
-        names = @string.split(NAMESPACE_SEPARATOR)
+        names = self.split(NAMESPACE_SEPARATOR)
         names.shift if names.empty? || names.first.empty?
         constant = Object
         names.each do |name|
@@ -67,33 +28,33 @@ module Tzispa
       end
 
       def self.camelize(str)
-        self.new(str).camelize
+        self.new(str.to_s).camelize
       end
 
       def camelize
-        self.class.new( @string.split(CLASSIFY_SEPARATOR).collect{ |w| w.capitalize }.join)
+        self.split(CLASSIFY_SEPARATOR).collect{ |w| w.capitalize }.join
       end
 
       def camelize!
-        @string = @string.split(CLASSIFY_SEPARATOR).collect!{ |w| w.capitalize }.join
+        self.split(CLASSIFY_SEPARATOR).collect!{ |w| w.capitalize }.join
       end
 
       def self.underscore(str)
-        self.new(str).underscore
+        self.new(str.to_s).underscore
       end
 
       def underscore
-        self.class.new(@string.dup.tap { |s|
+        self.dup.tap { |s|
           s.gsub!(NAMESPACE_SEPARATOR, UNDERSCORE_SEPARATOR)
           s.gsub!(/([A-Z\d]+)([A-Z][a-z])/, UNDERSCORE_DIVISION_TARGET)
           s.gsub!(/([a-z\d])([A-Z])/, UNDERSCORE_DIVISION_TARGET)
           s.gsub!(/[[:space:]]|\-/, UNDERSCORE_DIVISION_TARGET)
           s.downcase!
-        })
+        }
       end
 
       def underscore!
-        @string = @string.tap { |s|
+        self.tap { |s|
           s.gsub!(NAMESPACE_SEPARATOR, UNDERSCORE_SEPARATOR)
           s.gsub!(/([A-Z\d]+)([A-Z][a-z])/, UNDERSCORE_DIVISION_TARGET)
           s.gsub!(/([a-z\d])([A-Z])/, UNDERSCORE_DIVISION_TARGET)
@@ -102,48 +63,32 @@ module Tzispa
         }
       end
 
-
-      # Remove the accents from the string. Uses ACCENTS_MAPPING as the source map.
-      def removeaccents
-        str = String.new(self)
-        ACCENTS_MAPPING.each {|letter,accents|
-          packed = accents.pack('U*')
-          rxp = Regexp.new("[#{packed}]", nil, 'U')
-          str.gsub!(rxp, letter)
-        }
-
-        str
+      # Replace accents in the string using I18n.transliterate
+      def transliterate(locale=nil)
+        I18n.transliterate(self, ({locale: locale} if locale))
       end
 
       # Convert a string to a format suitable for a URL without ever using escaped characters.
-      # It calls strip, removeaccents, downcase (optional) then removes the spaces (optional)
+      # It calls strip, transliterate, downcase (optional) then removes the spaces (optional)
       # and finally removes any characters matching the default regexp (/[^-_A-Za-z0-9]/).
       #
       # Options
       #
       # * :downcase => call downcase on the string (defaults to true)
       # * :convert_spaces => Convert space to underscore (defaults to false)
-      # * :regexp => The regexp matching characters that will be converting to an empty string (defaults to /[^-_A-Za-z0-9]/)
+      # * :regexp => The regexp matching characters that will be removed (defaults to /[^-_A-Za-z0-9]/)
       def urlize(options = {})
         options[:downcase] ||= true
-        options[:convert_spaces] ||= false
+        options[:convert_spaces] ||= true
         options[:regexp] ||= /[^-_A-Za-z0-9]/
 
-        str = self.strip.removeaccents
-        str.downcase! if options[:downcase]
-        str.gsub!(/\ /,'_') if options[:convert_spaces]
-        str.gsub(options[:regexp], '')
+        self.transliterate(options[:locale]).strip.tap { |str|
+          str.downcase! if options[:downcase]
+          str.gsub!(/\ /,'_') if options[:convert_spaces]
+          str.gsub!(options[:regexp], '')
+        }
       end
 
-      private
-
-      def gsub(pattern, replacement = nil, &blk)
-        if block_given?
-          @string.gsub(pattern, &blk)
-        else
-          @string.gsub(pattern, replacement)
-        end
-      end
 
 
     end
