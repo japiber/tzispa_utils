9496c67b6ae9dc9ae50d4aa8b08aa38ad961e208
Juan Antonio Piñero <japinero@area-integral.com>
2017-02-20 10:49:21 +0100

    added hash refinement

---
+++ lib/tzispa/utils/duplicable.rb
@@ -0,0 +1,18 @@
+module Tzispa
+  module Utils
+    module Duplicable
+
+      def self.dup(value, &blk)
+        case value
+        when NilClass, FalseClass, TrueClass, Symbol, Numeric
+          value
+        when v = blk && blk.call(value)
+          v
+        else
+          value.dup
+        end
+      end
+      
+    end
+  end
+end
---
+++ lib/tzispa/utils/hash.rb
@@ -0,0 +1,57 @@
+require 'tzispa/utils/duplicable'
+
+module Tzispa
+  module Utils
+
+    DUPLICATE_LOGIC = proc do |value|
+      case value
+      when Hash
+        value.deep_dup
+      when ::Hash
+        Hash.new(value).deep_dup.to_h
+      end
+    end.freeze
+
+    refine Hash do
+
+      def symbolize!
+        keys.each do |k|
+          v = delete(k)
+          self[k.to_sym] = v
+        end
+
+        self
+      end
+
+      def deep_symbolize!
+        keys.each do |k|
+          v = delete(k)
+          v = self.class.new(v).deep_symbolize! if v.respond_to?(:to_hash)
+
+          self[k.to_sym] = v
+        end
+
+        self
+      end
+
+      def stringify!
+        keys.each do |k|
+          v = delete(k)
+          v = self.class.new(v).stringify! if v.respond_to?(:to_hash)
+
+          self[k.to_s] = v
+        end
+
+        self
+      end
+
+      def deep_dup
+        self.class.new.tap do |result|
+          @hash.each { |k, v| result[k] = Duplicable.dup(v, &DUPLICATE_LOGIC) }
+        end
+      end
+
+    end
+
+  end
+end
